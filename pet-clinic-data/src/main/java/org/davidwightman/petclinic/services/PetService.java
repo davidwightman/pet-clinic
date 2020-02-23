7fd18bf872123babeabe0e07ba99f79491df9e87
David Wightman <david@davidwightman.org>
2020-02-23 16:45:17 -0500

    refactor services to common crud service

--- pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package org.davidwightman.petclinic.services;
 
 import org.davidwightman.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package org.davidwightman.petclinic.services;
 
 import org.davidwightman.petclinic.model.Pet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Pet, Long> {
 
-public interface VetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
